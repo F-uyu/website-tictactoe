1d467599e5743a6867a7e38f683888bb34e31be9
Matthew Yu <matthewyu@matthews-mbp.lan>
2023-07-27 15:02:56 -0700

    Tic Tac Toe finished

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -21,19 +21,82 @@ export class MyRoom extends Room<MyRoomState> {
   }
 
   makeMove(sessionId: string, row: number, col: number){
-    console.log("ran")
     const currentPlayer = this.state.players.get(sessionId)
     if (currentPlayer && currentPlayer.symbol === this.state.turn && this.isCellEmpty(row, col)){
       const cellId = `${row}-${col}`
       this.state.board.get(cellId).space = currentPlayer.symbol
       this.state.turn = this.state.turn === "X" ? "O" : "X"
-      this.broadcast("updateBoard", this.state.board.toJSON())
     }
+    const winner = this.checkWinner()
+    const tie = this.checkTie()
+    if (winner) {
+      console.log("winner", winner)
+      this.resetBoard()
+    }
+    if (tie) {
+      console.log("tie")
+      this.resetBoard()
+    }
+    this.broadcast("updateBoard", this.state.board.toJSON())
   }
 
   isCellEmpty(row: number, col: number){
     const cellId = `${row}-${col}`
     return this.state.board.get(cellId).space === ""
+  }
+
+  checkWinner(): string | null {
+    const winningCombos = [
+      ["0-0", "0-1", "0-2"], // top row
+      ["1-0", "1-1", "1-2"], // middle row
+      ["2-0", "2-1", "2-2"], // bottom row
+      ["0-0", "1-0", "2-0"], // left column
+      ["0-1", "1-1", "2-1"], // middle column
+      ["0-2", "1-2", "2-2"], // right column
+      ["0-0", "1-1", "2-2"], // top left to bottom right
+      ["0-2", "1-1", "2-0"] // top right to bottom left
+    ];
+    for (const combination of winningCombos) {
+      const [cell1, cell2, cell3] = combination;
+      const value1 = this.state.board.get(cell1)?.space;
+      const value2 = this.state.board.get(cell2)?.space;
+      const value3 = this.state.board.get(cell3)?.space;
+  
+      if (value1 && value1 === value2 && value2 === value3) {
+        return value1; // Return the winning symbol (X or O)
+      }
+    }
+  
+    return null; // No winner found
+  }
+
+  checkTie(): boolean {
+    const board = this.state.board;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const cellId = `${i}-${j}`;
+        if (board.get(cellId)?.space === "") {
+          // Found an empty cell, the game is not a tie yet
+          return false;
+        }
+      }
+    }
+  
+    // If no empty cells were found, it's a tie
+    return true;
+  }
+
+  resetBoard() {
+    const board = this.state.board;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const cellId = `${i}-${j}`;
+        board.get(cellId).space = "";
+      }
+    }
+  
+    // Reset the current turn to "X" (assuming "X" starts first)
+    this.state.turn = "X";
   }
 
   onJoin (client: Client, options: any) {
